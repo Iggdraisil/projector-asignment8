74422f1a2f864931d39e5fa247e258500e559ee0
Oleh Paduchak <oleh@i3engineering.com>
2023-03-10 18:02:32 +0200

    implemented nginx homework

--- application/gen_url.py
+++
@@ -1,4 +0,0 @@
-if __name__ == '__main__':
-    urls = [f"http://localhost:8080/test?population={i}\n" for i in range(1000, 10_000_000, 5000)]
-    with open('urls.txt', 'w') as file:
-        file.writelines(urls)
--- load.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ab -n 1000000 -c 500 http://localhost:8082/test
